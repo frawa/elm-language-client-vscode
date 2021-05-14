7a931bb514aa495a1f4b385deccb1ec3b9b723bb
Frank Wagner <agilecoderfrank@gmail.com>
2021-05-14 21:08:35 +0200

    still trying to convice eslint

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -17,7 +17,7 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: "module",
     tsconfigRootDir: __dirname,
-    // project: ["./tsconfig.json"],
+    project: ["./tsconfig.json"],
   },
   plugins: ["@typescript-eslint", "prettier"],
   rules: {
